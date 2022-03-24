66f5257c2c5d16a40da85178d95f158ba2ff8ccf
Mike Zhang <winsource6@hotmail.com>
2022-03-24 16:08:41 +0000

    Fix Storage lib doesn't accept combine filters

--- FenicsDispatcher/FenicsFixError.cs
+++ FenicsDispatcher/FenicsFixError.cs
@@ -9,6 +9,7 @@ using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace FenicsDispatcher
 {
@@ -25,10 +26,10 @@ namespace FenicsDispatcher
 
             var tradeId = int.Parse(req.Query["TradeId"]);
 
-            var condition1 = TableQuery.GenerateFilterConditionForBool("IsProcessed", QueryComparisons.Equal, false);
-            var condition2 = TableQuery.GenerateFilterConditionForInt("TradeId", QueryComparisons.Equal, tradeId);
+            //var condition1 = TableQuery.GenerateFilterConditionForBool("IsProcessed", QueryComparisons.Equal, false);
+            var condition = TableQuery.GenerateFilterConditionForInt("TradeId", QueryComparisons.Equal, tradeId);
 
-            var condition = TableQuery.CombineFilters(condition1, TableOperators.And, condition2);
+            //var condition = TableQuery.CombineFilters(condition1, TableOperators.And, condition2);
 
             var query = new TableQuery<FenicsTaskEntity>().Where(condition);
 
@@ -37,8 +38,16 @@ namespace FenicsDispatcher
 
             var table = client.GetTableReference("FenicsTask");
 
-            var lst = await table.ExecuteQuerySegmentedAsync(query, null);
-            var task = lst.FirstOrDefault();
+            var lst = new List<FenicsTaskEntity>();
+            TableQuerySegment<FenicsTaskEntity> segment = null;
+
+            while (segment == null || segment.ContinuationToken != null)
+            {
+                segment = await table.ExecuteQuerySegmentedAsync(query, segment?.ContinuationToken);
+                lst.AddRange(segment.Results);
+            }
+
+            var task = lst.FirstOrDefault(t => t.IsProcessed == false);
 
 
             if (task == null)
