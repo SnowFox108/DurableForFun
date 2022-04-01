b85851c958d7a4c1c07aac2e58b5bb0bf3a177da
Mike.Zhang <mike.zhang@moneycorp.com>
2022-04-01 13:56:57 +0100

    Create default data in database.

--- FunctionDurable/Infrastructure/TradeRepository.cs
+++ FunctionDurable/Infrastructure/TradeRepository.cs
@@ -23,27 +23,30 @@ namespace FunctionDurable.Infrastructure
 
             var table = client.GetTableReference("IdGenerator");
 
-            await table.CreateIfNotExistsAsync();
+            var isFirstTime = await table.CreateIfNotExistsAsync();
 
-            //// Insert Data for Fist time only
-            //var tradeKey = new IdGeneratorEntity
-            //{
-            //    PartitionKey = "IdGenerator",
-            //    RowKey = "TradeGeneratorKey",
-            //    CounterId = 2000
-            //};
+            if (isFirstTime)
+            {
+                // Insert Data for Fist time only
+                var tradeKey = new IdGeneratorEntity
+                {
+                    PartitionKey = "IdGenerator",
+                    RowKey = "TradeGeneratorKey",
+                    CounterId = 2000
+                };
 
-            //var fenicsKey = new IdGeneratorEntity
-            //{
-            //    PartitionKey = "IdGenerator",
-            //    RowKey = "FenicsGeneratorKey",
-            //    CounterId = 5000
-            //};
+                var fenicsKey = new IdGeneratorEntity
+                {
+                    PartitionKey = "IdGenerator",
+                    RowKey = "FenicsGeneratorKey",
+                    CounterId = 5000
+                };
 
-            //TableOperation insertOperation = TableOperation.Insert(tradeKey);
-            //await table.ExecuteAsync(insertOperation);
-            //insertOperation = TableOperation.Insert(fenicsKey);
-            //await table.ExecuteAsync(insertOperation);
+                TableOperation insertOperation = TableOperation.Insert(tradeKey);
+                await table.ExecuteAsync(insertOperation);
+                insertOperation = TableOperation.Insert(fenicsKey);
+                await table.ExecuteAsync(insertOperation);
+            }
 
             var condition = TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, "TradeGeneratorKey");
             var query = new TableQuery<IdGeneratorEntity>().Where(condition);
